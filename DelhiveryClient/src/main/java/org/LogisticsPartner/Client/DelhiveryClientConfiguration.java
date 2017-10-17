f2c3abfee16e4e7f45d2c68ce8da47258223e947
Sabarinath Subramani <sabarinath_subramani@symantec.com>
2017-10-18 00:12:53 +0530

    url change

--- DelhiveryClient/src/main/java/org/LogisticsPartner/Client/DelhiveryClientConfiguration.java
+++ DelhiveryClient/src/main/java/org/LogisticsPartner/Client/DelhiveryClientConfiguration.java
@@ -2,7 +2,7 @@ package org.LogisticsPartner.Client;
 
 public class DelhiveryClientConfiguration {
 	
-	public static final String url = "https://godam.delhivery.com";
+	public static final String url = "https://api-godam.delhivery.com";
 	public static final String createOrdersPath = "/order/create/";
 	public static String cancelOrdersPath="/oms/api/update/CAN/";
 }
